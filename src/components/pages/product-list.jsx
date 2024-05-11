3e8bfc94c63d24ccf1870d4185aaad77f7a30890
TaiFilipelli <taielfilipelli@gmail.com>
2024-05-11 00:06:27 -0300

    new button and fixed routes

--- src/components/pages/product-list.jsx
+++ src/components/pages/product-list.jsx
@@ -1,10 +1,11 @@
-
+import ProductCard from "../product-card";
 const ProductList = () => {
   return (
     <div className=''>
       <h1 className='text-white'>Esto será una lista de productos</h1>  
+      <ProductCard/>
     </div>
   )
 }
 
-export default ProductList
+export default ProductList;
--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Button } from "keep-react"
+import { MagnifyingGlass } from "phosphor-react"
 
 const ProductCard = () => {
   return (
     <section className='border'>
-
+      <Button color="secondary" size="md">
+        Buscar
+        <MagnifyingGlass size={20} className='ml-1.5'/>
+      </Button>
     </section>
   )
 }
