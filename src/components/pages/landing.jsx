c7a8d9d3df6f3818d7cf71d6643daa231af40b76
TaiFilipelli <taielfilipelli@gmail.com>
2024-05-11 01:09:30 -0300

    Landing page finally ended.

--- src/components/pages/landing.jsx
+++ src/components/pages/landing.jsx
@@ -3,6 +3,7 @@ const Landing = () => {
   return (
     <main className='max-w-full '>
         <section className='text-white h-screen flex flex-col justify-center items-center'>
+            <img src="/public/logoMate.png" alt="Logo Mate" className='h-40 object-cover bg-gradient-to-r from-cyan-300 to-fuchsia-600 p-5 rounded-2xl'/>
             <h1 className='text-5xl font-bold p-8'>Bienvenido a Mate Libre!</h1>
             <h3 className='text-xl'>El hogar de todo argentino...y con mate incluido!</h3>
             <a href="/products" className='bg-gradient-to-r from-cyan-300 to-fuchsia-600 rounded-md w-60 p-5 font-bold my-9'>Poné el agua!</a>
