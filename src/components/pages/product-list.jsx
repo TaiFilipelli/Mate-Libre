216ef969c35097cfde11539aca6fe417668ef15b
TaiFilipelli <taielfilipelli@gmail.com>
2024-05-10 23:40:28 -0300

    vercel json changes

--- src/components/pages/product-list.jsx
+++ src/components/pages/product-list.jsx
@@ -2,7 +2,7 @@
 const ProductList = () => {
   return (
     <div className=''>
-      <h1 className='text-white'>Esto será una lista de productos</h1>
+      <h1 className='text-white'>Esto será una lista de productos</h1>  
     </div>
   )
 }
