f2efc2b384eaf37d02665dc4f83538eefc3b3dca
TaiFilipelli <taielfilipelli@gmail.com>
2024-05-15 00:04:43 -0300

    a bit of responsive

--- src/components/pages/landing.jsx
+++ src/components/pages/landing.jsx
@@ -22,7 +22,7 @@ const Landing = () => {
           <div className='left-auto mb-11' >
             <animated.div style={spring} className='mx-7'>
             <img src="/logoMate.png" alt="Logo Mate" className='h-48 bg-gradient-to-r from-cyan-300 to-fuchsia-600 rounded-md p-6'/>
-            <h1 className='text-6xl font-bold p-8 px-0 pb-5'>Bienvenido a Mate Libre </h1>
+            <h1 className='text-6xl font-bold p-8 px-0 pb-5 xs:text-3xl'>Bienvenido a Mate Libre </h1>
             <h3 className='text-2xl mb-14'>El hogar de todo argentino...y con mate incluido!</h3>
             <a href="/products" className='bg-gradient-to-r from-cyan-300 to-fuchsia-600 rounded-md w-60 p-5 font-bold px-16'>Poné el agua! </a> 
             <br /> <br /> <br />
