7fba39e3b00124b0380df365bd757f596f2eda75
TaiFilipelli <taielfilipelli@gmail.com>
2024-05-15 11:03:49 -0300

    upgraded anchors to Links & final README.md update (15-05)!!!

--- src/components/pages/landing.jsx
+++ src/components/pages/landing.jsx
@@ -1,6 +1,7 @@
 import { useSpring, animated } from "@react-spring/web";
 import { useEffect, useState } from "react";
 import DrawerMoreInfo from "../DrawerMoreInfo";
+import { Link } from "wouter";
 const Landing = () => {
   const [showLogo, setShowLogo]=useState(false);
 
@@ -24,7 +25,7 @@ const Landing = () => {
             <img src="/logoMate.png" alt="Logo Mate" className='h-48 bg-gradient-to-r from-cyan-300 to-fuchsia-600 rounded-md p-6'/>
             <h1 className='text-6xl font-bold p-8 px-0 pb-5 xs:text-3xl'>Bienvenido a Mate Libre </h1>
             <h3 className='text-2xl mb-14'>El hogar de todo argentino...y con mate incluido!</h3>
-            <a href="/products" className='bg-gradient-to-r from-cyan-300 to-fuchsia-600 rounded-md w-60 p-5 font-bold px-16'>Poné el agua! </a> 
+            <Link href="/products" className='bg-gradient-to-r from-cyan-300 to-fuchsia-600 rounded-md w-60 p-5 font-bold px-16'>Poné el agua! </Link> 
             <br /> <br /> <br />
             <DrawerMoreInfo/>
             </animated.div>
